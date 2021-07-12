f8d008580592cc4c1357a5979631e61ab70a733a
code9fair <mr.fair@msn.com>
2021-07-12 12:05:39 +0700

    Update sub route of plugin method

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
     port: 443,
     port_dev: 3443,
     port_live_reload: 3403,
+    port_ui_api: 3401,
     server_timeout: 10000,
     headers_mapping: {
       method: ':method',
